82a7fd9bcee75a9a24dc113f9f511fb670f31a06
Polina Kharionovskaya <polina14.05@yandex.ru>
2016-11-23 19:24:11 +0300

    +

--- js/script.js
+++ js/script.js
@@ -1,25 +1,26 @@
+var link = document.querySelector(".address .btn");
+var feedback = document.querySelector(".feedback");
+var close = feedback.querySelector(".close");
+var overlay = document.querySelector(".overlay");
 
+link.addEventListener("click", function(event) {
+  event.preventDefault();
+  feedback.classList.add("feedback_show");
+  overlay.classList.add("overlay_show");        
+});
 
-			var link = document.querySelector(".address .btn");
-			var feedback = document.querySelector(".feedback");
-			var close = feedback.querySelector(".close");
-      var overlay = document.querySelector(".overlay");
-			link.addEventListener("click", function(event) {
-        event.preventDefault();
-        feedback.classList.add("feedback_show");
-        overlay.classList.add("overlay_show");        
-      });
-			 close.addEventListener("click", function(event) {
-        event.preventDefault();
-        feedback.classList.remove("feedback_show");
-         overlay.classList.remove("overlay_show");  
-      });
-			 window.addEventListener("keydown", function(event) {
-        if (event.keyCode === 27) {
-          if (feedback.classList.contains("feedback_show")) {
-            feedback.classList.remove("feedback_show");
-            overlay.classList.remove("overlay_show");
-          }
-        }
-      });
+close.addEventListener("click", function(event) {
+  event.preventDefault();
+  feedback.classList.remove("feedback_show");
+  overlay.classList.remove("overlay_show");  
+});
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (feedback.classList.contains("feedback_show")) {
+       feedback.classList.remove("feedback_show");
+       overlay.classList.remove("overlay_show");
+    }
+  }
+});
 			
