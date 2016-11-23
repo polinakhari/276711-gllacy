e088e12f909060584226056d6e06b189fe80d1be
Polina Kharionovskaya <polina14.05@yandex.ru>
2016-11-23 18:37:43 +0300

    +

--- js/script.js
+++ js/script.js
@@ -3,22 +3,22 @@
 			var link = document.querySelector(".address .btn");
 			var feedback = document.querySelector(".feedback");
 			var close = feedback.querySelector(".close");
-      var overlay = document.querySelector(".index_wrapper");
+      var overlay = document.querySelector(".overlay");
 			link.addEventListener("click", function(event) {
         event.preventDefault();
         feedback.classList.add("feedback_show");
-        overlay.classList.add("overlay_js");        
+        overlay.classList.add("overlay_show");        
       });
 			 close.addEventListener("click", function(event) {
         event.preventDefault();
         feedback.classList.remove("feedback_show");
-         overlay.classList.remove("overlay_js");  
+         overlay.classList.remove("overlay_show");  
       });
 			 window.addEventListener("keydown", function(event) {
         if (event.keyCode === 27) {
           if (feedback.classList.contains("feedback_show")) {
             feedback.classList.remove("feedback_show");
-            overlay.classList.remove("overlay_js");
+            overlay.classList.remove("overlay_show");
           }
         }
       });
