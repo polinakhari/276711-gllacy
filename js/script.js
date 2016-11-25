8a4fcec01a34f58b5b88759b2e1d995048bd57db
Polina Kharionovskaya <polina14.05@yandex.ru>
2016-11-25 19:53:35 +0300

    +

--- js/script.js
+++ js/script.js
@@ -4,23 +4,22 @@ var close = feedback.querySelector(".close");
 var overlay = document.querySelector(".overlay");
 
 link.addEventListener("click", function(event) {
-  event.preventDefault();
-  feedback.classList.add("feedback_show");
-  overlay.classList.add("overlay_show");        
+    event.preventDefault();
+    feedback.classList.add("feedback_show");
+    overlay.classList.add("overlay_show");
 });
 
 close.addEventListener("click", function(event) {
-  event.preventDefault();
-  feedback.classList.remove("feedback_show");
-  overlay.classList.remove("overlay_show");  
+    event.preventDefault();
+    feedback.classList.remove("feedback_show");
+    overlay.classList.remove("overlay_show");
 });
 
 window.addEventListener("keydown", function(event) {
-  if (event.keyCode === 27) {
-    if (feedback.classList.contains("feedback_show")) {
-       feedback.classList.remove("feedback_show");
-       overlay.classList.remove("overlay_show");
+    if (event.keyCode === 27) {
+        if (feedback.classList.contains("feedback_show")) {
+            feedback.classList.remove("feedback_show");
+            overlay.classList.remove("overlay_show");
+        }
     }
-  }
 });
-			
